57e62249fb15e29389dfa07572e0f645ee13d41b
rahul8279 <rana827965@gmail.com>
2025-08-03 19:55:19 +0530

    final project

--- Backend/index.js
+++ Backend/index.js
@@ -8,11 +8,11 @@ import authRoutes from "./routes/user.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 import connectDB from "./lib/db.js";
-
+import path from "path";
 
 
 const PORT = process.env.PORT;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({limit:'5mb'}));
 app.use(express.urlencoded({extended:true,limit:'5mb'}))
@@ -27,6 +27,10 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get(/(.*)/,(req, res) => {
+  res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+})
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
