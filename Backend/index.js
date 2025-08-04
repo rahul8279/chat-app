3edc91be468da8e23034c531dc379e108d684580
rahul8279 <rana827965@gmail.com>
2025-08-04 20:01:22 +0530

    change variable

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-2tmo.onrender.com",
     credentials: true,
   })
 );
